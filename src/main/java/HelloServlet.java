8c107e78fd1edf004a2636acb30122dad6535ada
asoel <asoel@eadsys.de>
2016-06-01 15:40:50 +0200

    init version

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by asoel on 01.06.2016.
+ */
+@WebServlet(name="hello", urlPatterns = "/")
+public class HelloServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello Sepp");
+    }
+}
